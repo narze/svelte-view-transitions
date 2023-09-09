68c0091e90cfe6ffd9dbd9bddefe1464ac41e589
Manassarn Manoonchai <manassarn@gmail.com>
2023-09-09 15:31:36 +0700

    Add Astro-like fade transition animation

--- src/lib/transition.ts
+++ src/lib/transition.ts
@@ -1,28 +1,95 @@
-import type { U } from 'vitest/dist/reporters-cb94c88b.js';
-
 type ViewTransitionName = string;
+type ViewTransitionType = 'astroFade' | undefined;
 
-type TransitionOptions = ViewTransitionName;
+type TransitionOptions =
+	| ViewTransitionName
+	| {
+			name: ViewTransitionName;
+			type?: ViewTransitionType;
+	  };
 
 function isString(value: unknown): value is string {
 	return typeof value === 'string';
 }
 
 export function transition(node: HTMLElement, options: TransitionOptions) {
-	let name: string | undefined;
+	let name: ViewTransitionName;
+	let type: ViewTransitionType;
 
 	if (isString(options)) {
 		name = options;
+	} else {
+		name = options.name;
+		type = options.type;
 	}
 
-	if (name) {
-		node.style.setProperty('view-transition-name', options);
+	if (!name) {
+		throw new Error('Transition name is required');
+	}
+
+	// Normalize name
+	name = name.replace(/[^a-z0-9]/gi, '-').toLowerCase();
+
+	node.style.setProperty('view-transition-name', name);
+
+	if (type === 'astroFade') {
+		const duration = 180;
+
+		const css = `
+      :root {
+        --svelte-view-transition-duration--${name}: ${duration}ms;
+        --svelte-view-transition-timing-fn-astro: cubic-bezier(0.76, 0, 0.24, 1);
+      }
+
+      @keyframes svtFadeIn {
+        from {
+          opacity: 0;
+        }
+      }
+
+      @keyframes svtFadeOut {
+        to {
+          opacity: 0;
+        }
+      }
+
+      ::view-transition-old(${name}) {
+        animation-duration: var(--svelte-view-transition-duration--${name});
+        animation-timing-function: var(--svelte-view-transition-timing-fn-astro);
+        animation-fill-mode: both;
+        animation-name: svtFadeOut;
+      }
+
+      ::view-transition-new(${name}) {
+        animation-duration: var(--svelte-view-transition-duration--${name});
+        animation-timing-function: var(--svelte-view-transition-timing-fn-astro);
+        animation-fill-mode: both;
+        animation-name: svtFadeIn;
+      }
+    `;
+
+		const styleTag = document.querySelector(`style[data-svelte-view-transition-name="${name}"]`);
+
+		if (styleTag) {
+			styleTag.innerHTML = css;
+		} else {
+			document.head.insertAdjacentHTML(
+				'beforeend',
+				`<style data-svelte-view-transition-name="${name}">${css}</style>`
+			);
+		}
 	}
 
 	return {
 		destroy() {
-			if (name) {
-				node.style.removeProperty('view-transition-name');
+			node.style.removeProperty('view-transition-name');
+
+			const injectedStyle = document.querySelector(
+				`style[data-svelte-view-transition-name="${name}"]`
+			);
+
+			if (injectedStyle) {
+				injectedStyle.remove();
 			}
 		}
 	};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,4 @@
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/1" use:transition={'title'}>Link to another page</a>
+<a href="/1" use:transition={{ name: 'title', type: 'astroFade' }}>Link to another page</a>
